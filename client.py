0def997a2c5abf7e6bbc17c5bff508baadf59da8
ClovisLd <clovis.leden@gmail.com>
2025-05-15 08:28:51 +0200

    final

--- client.py
+++ client.py
@@ -10,11 +10,12 @@ from play import play, full_cell, Type_left
 #172.17.10.133
 server_ip = "localhost"
 port = int(sys.argv[1])
+turn = 0
 
 # pattern pour chercher le nom ou la position de l'erreur
 pattern = re.compile(r"\b([A-Z]{4}|\d{2})\b")
 
-
+# Etat du jeu et liste de piece restante
 pieces = ['SDEC', 'SDFP', 'SLEC', 'SLFP', 'BDFC', 'BDFP', 'BLEP', 'BDEP', 'SDFC', 'SLEP', 'SLFC', 'BLFP', 'BDEC', 'BLFC', 'SDEP', 'BLEC']
 board = []
 
@@ -34,30 +35,42 @@ with socket.socket() as client:
 
 
 def info_state(lives, errors, state, client):
-    global pieces, full_cell
+    global pieces, full_cell, turn
+        # retire la piece qu'on doit jouer de la liste piece
+    if state["piece"] in pieces:
+        Type_left(state["piece"])
+        pieces.remove(state["piece"])
+        
+    # regarde si il y a une erreure si il y en a une retirer la pieces de la liste piece
     if len(errors) != 0:
         print(f"\033[31m||Errors:||{errors}\033[0m")
         test = pattern.findall(errors[0]["message"])
         print(state["board"])
         if test:  # Check if test is not empty
             piece_to_remove = test[0]
+            test = []
             if piece_to_remove in pieces:
+                Type_left(piece_to_remove)
                 pieces.remove(piece_to_remove)
                 print(f"\033[92mRemoved {piece_to_remove} from pieces\033[0m")
-            elif state["piece"] in pieces:
-                pieces.remove(state["piece"])
+
     errors = 0
     board = state["board"]
+    print(turn)
     pieces = play(board, client, lives, state, pieces)
 
+    # remet tt a 0 quand une nouvelle partie commence
     if not full_cell:
-        pieces = ['SDEC', 'SDFP', 'SLEC', 'SLFP', 'BDFC', 'BDFP', 'BLEP', 'BDEP', 'SDFC', 'SLEP', 'SLFC', 'BLFP', 'BDEC', 'BLFC', 'SDEP', 'BLEC']
-        print("new party")
+        if turn > 2:
+            pieces = ['SDEC', 'SDFP', 'SLEC', 'SLFP', 'BDFC', 'BDFP', 'BLEP', 'BDEP', 'SDFC', 'SLEP', 'SLFC', 'BLFP', 'BDEC', 'BLFC', 'SDEP', 'BLEC']
+            print("new party")
+            turn = 0
     
     
 
 
 def Message_recieved(s:socket):
+    global turn
     # this function is called each time a message is recivied
     client, adress = s.accept()
     message = json.loads(client.recv(4000).decode())
@@ -70,6 +83,7 @@ def Message_recieved(s:socket):
     # respond to the play with "play"
     if message["request"] == "play":
         info_state(message["lives"], message["errors"], message["state"], client)
+        turn+=1
 
 
 
--- play.py
+++ play.py
@@ -9,9 +9,20 @@ message = ["Never gonna give you up",
            "Never gonna say goodbye",
            "Never gonna tell a lie and hurt you "]
 
+# gardue en local les endroit libre 
 empty_cell = set(range(16))
 full_cell = set()
-Typelf = np.array([4,4,4,4,4,4,4,4])
+
+# liste qui dit quelle caractérisitque sont les moins présentes
+Typelf = np.array([8,8,8,8,8,8,8,8])
+Letter = ["B","S","D","L","E","F","C","P"]
+pieces_score = {'SDEC': 0, 'SDFP': 0, 'SLEC': 0, 'SLFP': 0, 'BDFC': 0, 'BDFP': 0, 'BLEP': 0, 'BDEP': 0, 'SDFC': 0, 'SLEP': 0, 'SLFC': 0, 'BLFP': 0, 'BDEC': 0, 'BLFC': 0, 'SDEP': 0, 'BLEC': 0}
+
+def Max_indices(in_list:list, a:int, b:int):
+    if in_list[a] > in_list[b]:
+        return a
+    else:
+        return b
 
 def case_choice(board):
     global empty_cell, full_cell
@@ -32,41 +43,61 @@ def case_choice(board):
     
     full_cell -= b
     return rd.choice(list(empty_cell))
+
     
 def Type_left(Pe):
+    # reduit le score de chaque caractéristique des qu'elle est joué
+    global Typelf
     if Pe[0] == "B":
         Typelf[0]-=1
-    elif Pe[0] == "S":
+    if Pe[0] == "S":
         Typelf[1]-=1
-    elif Pe[1] == "D":
+    if Pe[1] == "D":
         Typelf[2]-=1
-    elif Pe[1] == "L":
+    if Pe[1] == "L":
         Typelf[3]-=1
-    elif Pe[2] == "E":
+    if Pe[2] == "E":
         Typelf[4]-=1
-    elif Pe[2] == "F":
+    if Pe[2] == "F":
         Typelf[5]-=1
-    elif Pe[3] == "C":
+    if Pe[3] == "C":
         Typelf[6]-=1
-    elif Pe[3] == "P":
-        Typelf[7]=1
+    if Pe[3] == "P":
+        Typelf[7]-=1
     
 def Piece_choice(pieces, board):
+    global pieces_score
+    for i in range(0,4):
+        lll = Letter[Max_indices(Typelf, 2*i , 2*i+1)]
+        for ppp in pieces:
+            print(f"\033[92mRemoved {Letter} from pieces\033[0m")
+            if ppp[i] == lll:
+                pieces_score[ppp] += 1
 
-    Pe = rd.choice(pieces)
+    print(pieces_score)
+    Pe = max(pieces_score, key=pieces_score.get)
+    print(Pe)
+    if Pe in board:
+        try:
+            pieces.remove(Pe)
+            Type_left(Pe)
+            Piece_choice(pieces, board)
+            return(Pe, pieces)
+        except:
+            pass
+    if Pe not in pieces:
+        pieces_score[Pe] = 0
+        Pe = max(pieces_score, key=pieces_score.get)
+    Type_left(Pe)
     pieces.remove(Pe)
+    pieces_score = {'SDEC': 0, 'SDFP': 0, 'SLEC': 0, 'SLFP': 0, 'BDFC': 0, 'BDFP': 0, 'BLEP': 0, 'BDEP': 0, 'SDFC': 0, 'SLEP': 0, 'SLFC': 0, 'BLFP': 0, 'BDEC': 0, 'BLFC': 0, 'SDEP': 0, 'BLEC': 0}
     return (Pe, pieces)
 
 
-def play(board, client,lives, state, pieces:list):
-    try:
-        if len(pieces) > 0:
-            choix, pieces = Piece_choice(pieces, board)
-            
-    except:
-        print(f"///{board}///")
-        
-    print(f"\033[92m{choix}\033[0m")
+
+def play(board, client, lives, state, pieces:list):
+    # envoit le move au serveur
+    choix, pieces = Piece_choice(pieces, board)
     client.send(json.dumps(
         {"response": "move",
         "move": {"pos": case_choice(board),
